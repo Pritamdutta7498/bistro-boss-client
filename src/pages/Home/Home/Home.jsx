934696924fa1c39ba73c2cde0010054304acd909
PritamDutta <webdevpritam7498@gmail.com>
2023-10-02 00:21:33 +0600

    router, layout,page setup

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-slate-900'>this is home</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam harum repudiandae nisi! Reprehenderit odio veritatis illo cum velit enim eveniet?</p>
+        </div>
+    );
+};
+
+export default Home;
