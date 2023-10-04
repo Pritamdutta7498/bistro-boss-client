bae620bed14ed6b1e68db9967f19cca4b1689ba6
PritamDutta <webdevpritam7498@gmail.com>
2023-10-04 13:28:56 +0600

    add inline style and complete the MenuItem section

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -14,12 +14,12 @@ const PopularMenu = () => {
       });
   }, []);
   return (
-    <section>
+    <section className="my-10">
       <SectionTitle
         subHeading={"---Check it out---"}
         heading={"FROM OUR MENU"}
       />
-      <div>
+      <div className="grid md:grid-cols-2 gap-10">
         {menu.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 const MenuItem = ({item}) => {
     const{name, recipe, image, price} = item;
-    console.log(name);
+    // console.log(name);
     return (
-        <div>
-            <h3>menu item</h3>
+        <div className='flex space-x-3 '>
+            <img src={image} alt="" style={{borderRadius: "0px 200px 200px 200px"}} className='w-[118px]' />
+            <div>
+                <h2 className='text-2xl font-semibold text-zinc-600'>{name}-----</h2>
+                <p className='  text-zinc-500'>{recipe}</p>
+            </div>
+            <p className='font-bold text-xl  text-orange-500'>${price}</p>
         </div>
     );
 };
