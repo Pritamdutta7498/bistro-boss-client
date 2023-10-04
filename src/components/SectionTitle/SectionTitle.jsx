61028f329c2ace574ef54e8c9cf649716862d4d1
PritamDutta <webdevpritam7498@gmail.com>
2023-10-04 11:01:28 +0600

    sectionTitle updated

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,14 +2,11 @@ import React from "react";
 
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div className="text-center my-10">
-      <p className="text-amber-400 font-semibold text-xl ">{subHeading}</p>
-
-      <hr className="border-b-4 w-2/6 mx-auto" />
-
-      <h2 className="py-4 text-4xl font-semibold text-zinc-700 uppercase">{heading}</h2>
-      <hr className="border-b-4 w-2/6 mx-auto" />
-
+    <div className="text-center my-10 md:w-4/12 mx-auto  font-semibold">
+      <p className="text-amber-400 text-xl ">{subHeading}</p>
+      <h2 className="py-4 text-4xl  border-y-4  text-zinc-700 uppercase">
+        {heading}
+      </h2>
     </div>
   );
 };
