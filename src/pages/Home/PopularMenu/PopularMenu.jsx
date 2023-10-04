c776160920fff9c9a82bf2221ad2982abb497b24
PritamDutta <webdevpritam7498@gmail.com>
2023-10-04 13:11:15 +0600

    useEffect, filter,map in PopularItem section

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // filtering the "popular" category data
+        const popularItem = data.filter((item) => item.category == "popular");
+        setMenu(popularItem);
+      });
+  }, []);
   return (
     <section>
       <SectionTitle
         subHeading={"---Check it out---"}
         heading={"FROM OUR MENU"}
       />
+      <div>
+        {menu.map((item) => (
+          <MenuItem key={item} item={item}></MenuItem>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MenuItem = () => {
+    return (
+        <div>
+            <h3>menu item</h3>
+        </div>
+    );
+};
+
+export default MenuItem;
