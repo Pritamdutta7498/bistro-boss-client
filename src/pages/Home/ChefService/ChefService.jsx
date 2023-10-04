22a10f8d2d6f4775402b39ade13a562920d7e6b8
PritamDutta <webdevpritam7498@gmail.com>
2023-10-04 11:34:21 +0600

    updated

--- src/pages/Home/ChefService/ChefService.jsx
+++ src/pages/Home/ChefService/ChefService.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import chefImg from "../../../assets/home/chef-service.jpg";
 const ChefService = () => {
   return (
-    <div className="my-10">
+   
       <div
         style={{ backgroundImage: `url(${chefImg})` }}
-        className="w-full bg-cover h-[600px]  flex justify-center items-center  mx-auto "
+        className="w-full bg-cover h-[600px]  flex justify-center items-center  mx-auto mb-10"
       >
         <div className="bg-white w-4/5 h-3/5 text-center p-20 ">
           <h2 className="text-4xl font-semibold font-serif text-zinc-700">
-            Bistro Boss
+            Bistro Boss Restaurant
           </h2>
           <p className="w-4/5 mx-auto my-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -20,7 +20,7 @@ const ChefService = () => {
           </p>
         </div>
       </div>
-    </div>
+    
   );
 };
 
---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"---Check it out---"}
+        heading={"FROM OUR MENU"}
+      />
+    </section>
+  );
+};
+
+export default PopularMenu;
