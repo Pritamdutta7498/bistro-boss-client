0acd025a6f7088b81db8c9956247e762bc0acc83
PritamDutta <webdevpritam7498@gmail.com>
2023-10-04 18:02:28 +0600

    updated

--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FaQuoteLeft } from "react-icons/fa";
 
 // Import Swiper styles
 import "swiper/css";
@@ -27,12 +28,13 @@ const Reviews = () => {
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
-            <div className="flex flex-col items-center m-16">
+            <div className="flex flex-col items-center mx-20 mb-10">
               <Rating
                 style={{ maxWidth: 180 }}
                 value={review.rating}
                 readOnly
               />
+              <FaQuoteLeft className="text-5xl my-3"/>
               <p className="my-3">{review.details}</p>
               <h1 className="text-semibold text-2xl text-yellow-500">{review.name}</h1>
             </div>
