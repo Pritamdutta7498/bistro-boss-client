bc0f59c448844669361f6cadc6e348f92ea2f27b
PritamDutta <webdevpritam7498@gmail.com>
2023-10-06 11:02:41 +0600

    using the react-parallax

--- src/pages/OurMenu/OurMenu.jsx
+++ src/pages/OurMenu/OurMenu.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
-import Cover from '../Shared/Cover/Cover';
-import menuImg from '../../assets/menu/banner3.jpg';
-import dessertImg from '../../assets/menu/dessert-bg.jpeg';
-import saladImg from '../../assets/menu/salad-bg.jpg';
-import PopularMenu from '../Home/PopularMenu/PopularMenu';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import Cover from "../Shared/Cover/Cover";
+import menuImg from "../../assets/menu/banner3.jpg";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
 
 const OurMenu = () => {
   return (
-    <div className='py-20'>
+    <div className="py-20">
       <Helmet>
         <title>Bistro Boss | Menu</title>
         <link rel="canonical" href="https://www.tacobell.com/" />
       </Helmet>
-      <Cover img={menuImg} title={"Our Menu"}/>
+      <Cover img={menuImg} title={"Our Menu"} />
       {/* for test we use the popular menu section*/}
-      <PopularMenu/>
-      <Cover img={dessertImg} title={"Our Menu Dessert"}/>
-      <PopularMenu/>
-      <Cover img={saladImg} title={"Our Menu salad"}/>
-
-
+      <PopularMenu />
+      <Cover img={dessertImg} title={"Our Menu Dessert"} />
+      <PopularMenu />
+      <Cover img={saladImg} title={"Our Menu salad"} />
     </div>
   );
 };
--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import { Parallax, Background } from "react-parallax";
 
-const Cover = ({img, title}) => {
+const Cover = ({ img, title }) => {
   return (
-    <div
-      className="hero h-[600px]"
-      style={{
-        backgroundImage:
-          `url("${img}")`,
-      }}
+    <Parallax
+      blur={{ min: -30, max: 15 }}
+      bgImage={img}
+      bgImageAlt="the img"
+      strength={-200}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-2xl p-10 bg-black bg-opacity-40 rounded-md">
-          <h1 className="text-5xl font-bold text-white uppercase my-2">{title}</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, mollitia velit veniam facere quisquam accusantium vero, explicabo consectetur aliquam molestias voluptatum laudantium, saepe accusamus quod sunt veritatis ad culpa iusto.</p>
+      <div className="hero h-[600px]">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-2xl p-10 bg-black bg-opacity-40 rounded-md">
+            <h1 className="text-5xl font-bold text-white uppercase my-2">
+              {title}
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Expedita, mollitia velit veniam facere quisquam accusantium vero,
+              explicabo consectetur aliquam molestias voluptatum laudantium,
+              saepe accusamus quod sunt veritatis ad culpa iusto.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
