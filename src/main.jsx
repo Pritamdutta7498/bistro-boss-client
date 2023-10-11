404a1bbd3033971bc2ab9f069361947536908eee
PritamDutta <webdevpritam7498@gmail.com>
2023-10-11 11:35:57 +0600

    using the AuthProvider in main file

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="max-w-screen-xl mx-auto">
     <React.StrictMode>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-      </HelmetProvider>
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </AuthProvider>
     </React.StrictMode>
     ,
   </div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,7 +1,17 @@
-import React, { createContext } from "react";
-export const AuthContext = createContext(null);//must be export
-const AuthProvider = () => {
-  return <div></div>;
+import React, { createContext, useState } from "react";
+export const AuthContext = createContext(null); //must be export
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
