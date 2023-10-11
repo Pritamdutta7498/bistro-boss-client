1b2eefb896801b09f2ef0eca57cd4c7c84c049a4
PritamDutta <webdevpritam7498@gmail.com>
2023-10-11 11:30:22 +0600

    creating a provider

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,7 @@
+import React, { createContext } from "react";
+export const AuthContext = createContext(null);//must be export
+const AuthProvider = () => {
+  return <div></div>;
+};
+
+export default AuthProvider;
