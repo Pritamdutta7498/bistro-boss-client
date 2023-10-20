2604f8e304d71b60971668d007499d8da08fe794
PritamDutta <webdevpritam7498@gmail.com>
2023-10-20 11:39:44 +0600

    using reduce function for added the total price

--- src/pages/Dashboard/MyCart.jsx
+++ src/pages/Dashboard/MyCart.jsx
@@ -5,6 +5,9 @@ import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
 const MyCart = () => {
   const [cart] = useCart();
+  // console.log(cart[1].price);
+  const totalPrice = cart.reduce((sum, item) => item.price + sum, 0);
+  console.log(totalPrice);
   return (
     <>
       <Helmet>
@@ -15,7 +18,7 @@ const MyCart = () => {
 
       <div className="w-[972px] h-[972px] flex  justify-between p-10 " >
         <h2 className="text-3xl font-bold">TOTAL ORDERS: {cart.length}</h2>
-        <p className="text-3xl font-bold">TOTAL PRICE: ${}</p>
+        <p className="text-3xl font-bold">TOTAL PRICE: ${totalPrice}</p>
        <button className="btn btn-info text-white font-bold">PAY</button>
 
       </div>      
