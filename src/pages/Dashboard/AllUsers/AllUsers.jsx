5da1e23b52bc9fa1b0607a0f0127b04e006c8d11
PritamDutta <webdevpritam7498@gmail.com>
2024-01-22 00:05:00 +0600

    showing all menu with tanstack query simple

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -2,15 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const AllUsers = () => {
-  const { data: users = [], refetch } = useQuery(['users'], async () => {
-    const res = await fetch('http://localhost:5000/users');
+//   const { data: users = [], refetch } = useQuery(['users'], async () => {
+//     const res = await fetch('http://localhost:5000/users');
+//     console.log(res)
+//     return res.json();
+//   });
+  const { data: menu = [], refetch } = useQuery(['menu'], async () => {
+    const res = await fetch('http://localhost:5000/menu');
     console.log(res)
     return res.json();
   });
 
   return (
     <div>
-      <h2>All users: {users.length}</h2>
+      <h2>All users: {menu.length}</h2>
     </div>
   );
 };
