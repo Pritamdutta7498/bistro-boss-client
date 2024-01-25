4d0972ed39be41726045df83f0196f3bef0c4e14
PritamDutta <webdevpritam7498@gmail.com>
2024-01-25 15:46:28 +0600

    Replace `useContext` and `fetch` with `useAuth` and `axiosSecure`

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,21 +1,21 @@
+// api, axios (axios secure), tan stack 
+
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
-import { AuthContext } from "../providers/AuthProvider";
-const useCart = (email) => {
-  const { user } = useContext(AuthContext);
-  const token = localStorage.getItem("access-token")
-  const { refetch, data: cart = [] } = useQuery({
-    queryKey: ["carts", user?.email],
-    queryFn: async () => {
-      const res = await fetch(
-        `http://localhost:5000/carts?email=${user?.email}`, {headers:{
-          authorization: `bearer ${token}`
-        }}
-      );
-      return res.json();
-    },
-  });
-  return [cart, refetch];
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
+
+const useCart = () => {
+    const axiosSecure = useAxiosSecure();
+    const { user} = useAuth();
+    const { refetch, data: cart = [] } = useQuery({
+        queryKey: ['cart', user?.email],
+        queryFn: async() => {
+            const res = await axiosSecure.get(`/carts?email=${user.email}`);
+            return res.data;
+        }
+    })
+
+    return [cart, refetch]
 };
 
 export default useCart;
